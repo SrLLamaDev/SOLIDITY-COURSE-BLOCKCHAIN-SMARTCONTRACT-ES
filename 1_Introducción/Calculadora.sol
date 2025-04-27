83662afba26a1255f2fc3a4b1577fcfbe66499b0
SrLLamaDev <srllamadev@gmail.com>
2025-04-26 22:53:30 -0400

    uwu

---
+++ 1_Introducción/Calculadora.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract CalculadoraBasica {
+    //uint => 0 hacia adelante
+    function suma(int a, int b) public pure returns(int){
+        return a + b;
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    function resta(int a, int b) public pure returns(int){
+        return a - b;
+    }
+
+    function multiplica(int a, int b) public pure returns(int){
+        return a * b;
+    }
+
+    function divide(int a, int b) public pure returns(int){
+        return a / b;
+    }
+}
