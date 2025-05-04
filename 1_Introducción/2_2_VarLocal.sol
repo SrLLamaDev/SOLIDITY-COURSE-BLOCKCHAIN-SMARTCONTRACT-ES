8ec63e80198522443b877fccf4b5b3c302a7dcc6
SrLLamaDev <srllamadev@gmail.com>
2025-05-03 21:59:18 -0400

    uwu

--- 1_Introducción/2_2_VarLocal.sol
+++ 1_Introducción/2_2_VarLocal.sol
@@ -1,14 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-/**
- * Son variables temporales usadas dentro de funciones
- * Existen mientras se ejecuta la función
- */
-contract VarLocal {
-    
-    function sumar(uint a, uint b) public pure returns (uint) {
-        uint resultado = a + b; // Variable local
+pragma solidity 0.8.26;
+/*
+VARIABLES LOCALES
+-   son variables temporales que son usadas dentro de
+    las funciones
+-   Estas variables existen mientras se ejecuta la funci[on
+*/
+contract VarLocal{
+    //uint 0, 1, 2, 3 ...
+    //int ... -3, -2, -1, 0, 1, 2, 3... 
+    function sumar(uint a, uint b) public pure returns (uint){
+        //incio
+        uint c = 3;
+        uint resultado = a + b + c; //variable local
         return resultado;
+        //fin
     }
-
 }
