95a31fd440af29ac5a920fcc3aad8a466a1b7105
SrLLamaDev <srllamadev@gmail.com>
2025-05-11 00:48:20 -0400

    asd

--- 4_Modificadores/ModiFuncPayable.sol
+++ 4_Modificadores/ModiFuncPayable.sol
@@ -1,13 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
 /*
-Permite que una función reciba ether.
-Útil para pagos, compras, etc.
+        PAYABLE / PAGADERA
+Nos permite recibir
+UTILIDAD: pagos, compras ...
 */
 contract ModiFuncPayable {
-    
-    function recibirPago() public payable {
-    // Puedes recibir ether en esta función
-    }
+    //evento para registrar pagos recibidos
+    event PagoRecibido(address remitente, uint monto);
 
+    function recibirPago() public payable{
+        require(msg.value>0, "Deber enviar algo de activos digitales");
+        emit PagoRecibido(msg.sender, msg.value);
+    }
+    //funcion para consultar el balance
+    function verBalance() public view returns (uint){
+        return address(this).balance;
+    }
 }
